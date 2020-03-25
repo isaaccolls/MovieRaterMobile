13d9cc7ce0dba577138e18db1d3bdcc8b70703ff
Isaac Colls <colls_isaac@hotmail.com>
2020-03-25 14:25:14 -0300

    font awesome installed 🤔

--- components/detail.js
+++ components/detail.js
@@ -1,36 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function Detail({ route, navigation }) {
   const { movie } = route.params;
 
   return (
     <View>
-      <Text>Detail about {movie.title}</Text>
+      <Text>{movie.title}</Text>
+      <FontAwesomeIcon icon={ faStar } />
+      <Text>{movie.description}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  item: {
-    flex: 1,
-    padding: 10,
-    height: 50,
-    backgroundColor: '#282C35',
-  },
-  itemText: {
-    color: '#FFFFFF',
-    fontSize: 24,
-  },
-  logo: {
-    width: '100%',
-    height: 135,
-    paddingTop: 30,
-  },
-});
+const styles = StyleSheet.create({});
