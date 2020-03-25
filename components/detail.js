da0741b81ca573d7c97ea4b732ca52400eac1b04
Isaac Colls <colls_isaac@hotmail.com>
2020-03-25 14:52:27 -0300

    Icons for rating

--- components/detail.js
+++ components/detail.js
@@ -7,11 +7,30 @@ export default function Detail({ route, navigation }) {
 
   return (
     <View>
-      <Text>{movie.title}!</Text>
-      <FontAwesomeIcon icon={["fas","star"]} />
+      <Text>{movie.title}</Text>
+      <View style={styles.starContainer}>
+        <FontAwesomeIcon style={movie.avg_rating > 0 ? styles.orange : styles.white} icon={["fas","star"]} />
+        <FontAwesomeIcon style={movie.avg_rating > 1 ? styles.orange : styles.white} icon={["fas","star"]} />
+        <FontAwesomeIcon style={movie.avg_rating > 2 ? styles.orange : styles.white} icon={["fas","star"]} />
+        <FontAwesomeIcon style={movie.avg_rating > 3 ? styles.orange : styles.white} icon={["fas","star"]} />
+        <FontAwesomeIcon style={movie.avg_rating > 4 ? styles.orange : styles.white} icon={["fas","star"]} />
+        <Text>({movie.no_of_ratings})</Text>
+      </View>
       <Text>{movie.description}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  starContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  orange: {
+    color: "orange",
+  },
+  white: {
+    color: "white",
+  },
+});
